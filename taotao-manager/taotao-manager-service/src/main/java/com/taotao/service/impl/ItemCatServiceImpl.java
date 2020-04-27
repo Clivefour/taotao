d27da6563bbaf345751ea1ce18059917e7969282
grts <bao97664340@qq.com>
2020-04-27 15:34:53 +0800

    商品分类展示与bug解决

--- taotao-manager/taotao-manager-service/src/main/java/com/taotao/service/impl/ItemCatServiceImpl.java
+++ taotao-manager/taotao-manager-service/src/main/java/com/taotao/service/impl/ItemCatServiceImpl.java
@@ -1,6 +1,8 @@
 package com.taotao.service.impl;
 
 import com.taotao.mapper.TbItemCatMapper;
+import com.taotao.pojo.ItemCat;
+import com.taotao.pojo.ItemCatResult;
 import com.taotao.pojo.TbItemCat;
 import com.taotao.pojo.ZtreeResult;
 import com.taotao.service.ItemCatService;
@@ -25,5 +27,49 @@ public class ItemCatServiceImpl implements ItemCatService {
             results.add(result);
         }
         return results;
+    }
+
+    @Override
+    public ItemCatResult getItemCats() {
+        ItemCatResult result = new ItemCatResult();
+        result.setData(getItemCatList(0L));
+        return result;
+    }
+    //
+    private List<?> getItemCatList(Long parentId){
+        // parentId = 0
+        int count = 0;
+        List list = new ArrayList();
+        List<TbItemCat> tbItemCats = tbItemCatMapper.findTbItemCatByParentId(parentId);
+        //遍历一级类目
+        for (TbItemCat itemCat:tbItemCats) {
+            //   id = 1        name = 图书、音像、电子书刊
+            ItemCat item = new ItemCat();
+            //判断他只父级目录还是最底级目录
+            if(itemCat.getIsParent()){
+                //代表一定是第一级或者第二级类目
+                item.setUrl("/products/"+itemCat.getId()+".html");
+                if(itemCat.getParentId()==0){
+                    //"n":"<a href='/products/1.html'>图书、音像、电子书刊</a>",
+                    item.setName("<a href='/products/"+itemCat.getId()+".html'>"+itemCat.getName()+"</a>");
+                }else{
+                    //一定是第二级类目
+                    item.setName(itemCat.getName());
+                }
+                    //设置在设置i
+                    item.setItem(getItemCatList(itemCat.getId()));
+                    //
+                    list.add(item);
+                    count++;
+                if (parentId == 0 && count >= 14) {
+                    break;
+                }
+
+            }else{
+                //一定是最底级目录 一定是三级类目
+                list.add("/products/"+itemCat.getId()+".html|"+itemCat.getName());
+            }
+        }
+        return list;
     }
 }
